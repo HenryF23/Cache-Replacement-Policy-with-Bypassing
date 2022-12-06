c5541c9648f70292e2f6489d745f9c5df3502f04
henry <henry@cs-arch-srv03.com>
2022-12-06 14:37:36 -0800

    update slru and bypass

--- replacement/llc_replacement.cc
+++ replacement/llc_replacement.cc
@@ -1,58 +1,16 @@
 #include "cache.h"
 
-// * SLRU
-#include <cstdlib>
-#include <time.h>
-
-#define RANDOM_PROMOTION_PROB 32 // p = 1/32 gets promoted
-#define RANDOM_PROMOTION_ACTIVE true
-#define AGING_ACTIVE true
-
 // initialize replacement state
 void CACHE::llc_initialize_replacement()
 {
-    srand(time(0));
+
 }
 
 // find replacement victim
 uint32_t CACHE::llc_find_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
 {
-    uint32_t way = 0;
-
-    for (way = 0; way < NUM_WAY; way++) {
-        if (block[set][way].valid == false) {
-            return way;
-        }
-    }
-
-    if (way == NUM_WAY) {
-        uint32_t victimBlock = 0;
-        uint32_t victimBlockSLRU = 0;
-        for (way = 0; way < NUM_WAY; way++) {
-            if (block[set][way].current_group == prob && (block[set][way].slru >= victimBlockSLRU)) {
-                victimBlock = way;
-                victimBlockSLRU = block[set][way].slru;
-            }
-        }
-
-        if (block[set][victimBlock].current_group == prob)
-            return victimBlockSLRU;
-
-        victimBlock = 0;
-        victimBlockSLRU = 0;
-        for (way = 0; way < NUM_WAY; way++) {
-            if (block[set][way].current_group == prot && (block[set][way].slru >= victimBlock)) {
-                victimBlock = way;
-            }
-        }
-
-        if (block[set][victimBlock].current_group == prot)
-            return victimBlockSLRU;
-    }
-
-    std::cout << "DEBUG: No victim block found!\n";
-
-    return 0; // Normally. this line should not be executed
+    // baseline LRU
+    return lru_victim(cpu, instr_id, set, current_set, ip, full_addr, type); 
 }
 
 // called on every cache hit and cache fill
@@ -82,50 +40,14 @@ void CACHE::llc_update_replacement_state(uint32_t cpu, uint32_t set, uint32_t wa
     // cout << "CPU: " << cpu << "  LLC " << setw(9) << TYPE_NAME << " set: " << setw(5) << set << " way: " << setw(2) << way;
     // cout << hex << " paddr: " << setw(12) << paddr << " ip: " << setw(8) << ip << " victim_addr: " << victim_addr << dec << endl;
 
-    if (hit && (type == WRITEBACK)) // writeback hit does not update SLRU state
+    // baseline LRU
+    if (hit && (type == WRITEBACK)) // writeback hit does not update LRU state
         return;
 
-    if (!hit && (type == LOAD || type == RFO)) { // miss, newly allocated line
-        // aging
-        if (AGING_ACTIVE) {
-            uint32_t tempSLRU = 0;
-            for (uint32_t i = 0; i < way; i++)
-            {
-                if (i != way && block[set][way].valid && block[set][way].current_group == prot)
-                {
-                    if (block[set][way].slru >= tempSLRU)
-                        tempSLRU = i;
-                }
-            }
-
-            if (block[set][tempSLRU].valid && block[set][tempSLRU].current_group == prot)
-            {
-                block[set][way].current_group = prob;
-                slru_update(set, way);
-            }
-        }
-
-        // random promotion
-        if (RANDOM_PROMOTION_ACTIVE) {
-            if (RANDOM_PROMOTION_ACTIVE && rand() % 100 < RANDOM_PROMOTION_PROB)
-                block[set][way].current_group = prot;
-            else
-                block[set][way].current_group = prob;
-
-            slru_update(set, way);
-        }
-
-        // TODO: add virtual bypass
-    }
-
-    if (type != WRITEBACK && hit) {
-        // move to protected
-        block[set][way].current_group = prot;
-        slru_update(set, way);
-    }
+    return lru_update(set, way);
 }
 
 void CACHE::llc_replacement_final_stats()
 {
-    
+
 }
